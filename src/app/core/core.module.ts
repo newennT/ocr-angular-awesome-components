5836ec2bef5f2b067e9fdd21a268eb71f3a581bf
NewennT <newenn.turbiau@gmail.com>
2026-01-12 15:35:57 +0100

    Base

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +11,12 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    RouterModule
+],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
