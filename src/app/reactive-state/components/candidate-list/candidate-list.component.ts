0783508530a954269fc66cc4290c8883d4df89db
NewennT <newenn.turbiau@gmail.com>
2026-01-19 11:34:26 +0100

    Management réactif

---
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -0,0 +1,64 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { map, Observable, startWith } from 'rxjs';
+import { CandidatesService } from '../../services/candidates.service';
+import { Candidate } from '../../models/candidate.model';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { CandidateSearchType } from '../../enums/candidate-search-type.enum';
+import { combineLatest } from 'rxjs';
+
+@Component({
+  selector: 'app-candidate-list',
+  templateUrl: './candidate-list.component.html',
+  styleUrls: ['./candidate-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CandidateListComponent implements OnInit {
+
+  loading$!: Observable<boolean>;
+  candidates$!: Observable<Candidate[]>
+  searchCtrl!: FormControl;
+  searchTypeCtrl!: FormControl;
+
+  searchTypeOptions!:{
+    value: CandidateSearchType,
+    label: string
+  }[];
+
+  constructor(private candidatesService: CandidatesService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+    this.initObservables();
+    this.candidatesService.getCandidatesFromServer();
+  }
+
+  private initObservables() {
+    this.loading$ = this.candidatesService.loading$;
+    const search$ = this.searchCtrl.valueChanges.pipe(
+      startWith(this.searchCtrl.value),
+      map(value => value.toLowerCase())
+    )
+    const searchType$: Observable<CandidateSearchType> = this.searchTypeCtrl.valueChanges.pipe(
+      startWith(this.searchTypeCtrl.value)
+    )
+    // this.candidates$ = this.candidatesService.candidates$;
+    this.candidates$ = combineLatest([
+      search$,
+      searchType$,
+      this.candidatesService.candidates$
+    ]).pipe(
+      map(([ search, searchType, candidates]) => candidates.filter(candidate => candidate[searchType].toLowerCase().includes(search as string)))
+    )
+  }
+
+  private initForm(){
+    this.searchCtrl = this.formBuilder.control('');
+    this.searchTypeCtrl = this.formBuilder.control(CandidateSearchType.LASTNAME);
+    this.searchTypeOptions = [
+      { value: CandidateSearchType.LASTNAME, label: 'Nom' },
+      { value: CandidateSearchType.FIRSTNAME, label: 'Prénom' },
+      { value: CandidateSearchType.COMPANY, label: 'Entreprise' }
+    ]
+  }
+
+}
